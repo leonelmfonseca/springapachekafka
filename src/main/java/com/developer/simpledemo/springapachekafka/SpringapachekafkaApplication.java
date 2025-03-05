2c5a2eec3b382acd579552c038c3ada7c4f417a2
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-05 23:33:33 +0000

    feat: initial commit

---
+++ src/main/java/com/developer/simpledemo/springapachekafka/SpringapachekafkaApplication.java
@@ -0,0 +1,13 @@
+package com.developer.simpledemo.springapachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapachekafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapachekafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developer/simpledemo/springapachekafka/SpringapachekafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.simpledemo.springapachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapachekafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
